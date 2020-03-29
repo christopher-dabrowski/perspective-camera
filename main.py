b64711526c73a8aacdad95a45d3fd4c5b4db7c67
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-29 19:54:12 +0200

    Display window

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pygame
 
 print("Hi")
+
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('3D models :D')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
