e849f34a7b1ae277821191f23735ca31e740b925
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-29 20:22:24 +0200

    Adding nodes to wireframe

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
+import numpy as np
+from wireframe import Wireframe
 
 print("Hi")
 
 screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('3D models :D')
+
+wireframe = Wireframe()
+wireframe.add_nodes(np.array([[20, 20, 20],
+                              [100, 20, 20]]))
+
+print(wireframe.list_nodes())
 
 running = True
 while running:
--- wireframe.py
+++ wireframe.py
@@ -7,6 +7,21 @@ class Wireframe:
     def __init__(self):
         super().__init__()
         # Nodes are stored in single row matrix to allow fast matrix operations
-        self.nodes = np.zeros((0, 4))  # Format: (x, y, z, 1)
+        self.nodes = np.zeros([0, 4])  # Format: (x, y, z, 1)
         # Edges are list of tuples that are numbers of given nodes
         self.edges = []
+
+    def add_nodes(self, nodes: np.array) -> None:
+        """Add new points to the bottom of the array"""
+        print(nodes.shape[1])
+        print(np.ones([nodes.shape[0], 1]))
+        with_ones = np.hstack([nodes, np.ones([nodes.shape[0], 1])])
+        self.nodes = np.vstack([self.nodes, with_ones])
+
+    def list_nodes(self) -> str:
+        result = "Nodes list:\n"
+        for row in range(self.nodes.shape[0]):
+            node = self.nodes[row]
+            result += f'{node}\n'
+
+        return result
